f76dba291bcd2dd7a8224f181efdc8b7fc1ec7f5
Shane Daly <shane.daly@wpengine.com>
2024-07-24 15:06:34 +0100

    chore: add similarity api usage (#1)

--- src/app/utils/context.ts
+++ src/app/utils/context.ts
@@ -1,25 +1,42 @@
+// These are the types that are used in the `getContext` function
+type Doc = {
+  id: string;
+  data: Record<string, any>;
+  score: number;
+};
+
+type Similarity = {
+  total: number;
+  docs: Doc[];
+};
+
+type Response = {
+  data: {
+    similarity: Similarity;
+  };
+};
+
 // The function `getContext` is used to retrieve the context of a given message
-export const getContext = async (
-  message: string
-): Promise<Record<string, any>> => {
+export const getContext = async (message: string): Promise<Response> => {
   const url = process.env.SMART_SEARCH_URL ?? "";
   const token = process.env.SMART_SEARCH_ACCESS_TOKEN ?? "";
 
-  const query = `
-    {
-        find(
-            query: "${message}"
-            semanticSearch: { searchBias: 10, fields: ["post_content"] }
-        ) { 
-            total
-            documents {
-                id
-                score
-                data
-            }
+  const query = `{
+    similarity(
+      input: {
+        nearest: {
+          text: "${message}",
+          field: "post_content"
         }
+      }) {
+      total
+      docs {
+        id
+        data
+        score
+      }
     }
-  `;
+  }`;
 
   const response = await fetch(url, {
     method: "POST",
@@ -30,7 +47,5 @@ export const getContext = async (
     body: JSON.stringify({ query }),
   });
 
-  const data = await response.json();
-
-  return data;
+  return await response.json();
 };

af56e9daf842745048e3cceccc24799b0d48e0b5
Shane Daly <shane.daly@wpengine.com>
2025-06-11 15:16:02 +0100

    chore: use simple graphql client

--- src/graphql/types.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLError } from "graphql";
-
-export interface GraphqlResponse<T> {
-  data: T;
-  errors?: GraphQLError[] | undefined;
-  extensions?: any;
-  headers: Headers;
-  status: number;
-}
